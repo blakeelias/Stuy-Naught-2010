39e0af71e1b56cf0a3c1a130c03afdb0b8c7869e
Blake Elias <blakeelias@gmail.com>
2010-10-10 00:18:50 -0400

    simple-retrieve.c: restructured to use state variable, starts at the real initial position

--- strategies/simple-retrieve.c
+++ strategies/simple-retrieve.c
@@ -8,55 +8,82 @@
  *        panelradius: 0.3
  *        havepanel: 0
  *    Positioning:
- *        SPH1: X=0.7, Y=0, Z=0
- *        SPH2: X=0,   Y=0, Z=0
+ *        SPH1: X=0, Y=0.3484, Z=0
+ *        SPH2: X=0,   Y=-0.3484, Z=0
  *
  * Result codes:
  *    SPH1 = 20 (I won)
  *    SPH2 = 40 (Other sat won)
  *
- * Total time: 165 seconds
+ * Total time: 94.0 seconds
+ * Fuel used: 67% (33% remaining)
  *
  * Description of behavior:
- *    SPHERE 1 (blue) starts in the center of panel initialization circle
- *    (relies on initial positioning), turns towards solar panel (relies on
- *    initial panel position in Game Variables).  Moves forward, docks to panel,
+ *    SPHERE 1 (blue) starts in the satellite initialization circle, moves
+ *    to solar panel initialization circle, and immediately sees the panel
+ *    (relies on initial panel position).  Moves forward, docks to panel,
  *    travels to station and docks panel to the station.
- *
- *    During trip from panel circle to the station, a near collision with the
- *    other SPHERE results in it moving away, using up its fuel.
  * 
  */
 
+int state = 0;
+
 void ZRUser(float * myState, float * otherState, float time) {
-    if (time < 150) {
-        //TODO: move to center of panel initialization circle, instead of
-        //assuming that position from the start
+    printf("%d\n", state);
+    
+    if (state == 0) {
+        float attitude[3] = {0, 1, 0};
+        float panel_distance;
+        float panel_center[3] = {.7, 0, 0};
+        
+        ZRSetPositionTarget(panel_center);
+        ZRSetAttitudeTarget(attitude);
+        
+        panel_distance = Vfunc(6, myState, panel_center, NULL, 0);
+        if (panel_distance < 0.01)
+            state = 2;
+    }
+    //TODO: rotate around the circle to find the panel; in this case,
+    //panel angle is 0 so we automatically see it after the attitude change
+    if (state == 1);
+    if (state == 2) {
         float attitude[3] = {0, 1, 0};
         ZRSetAttitudeTarget(attitude);
-        //TODO: rotate around the circle to find the panel; in this case,
-        //panel angle is 0 so we automatically see it after the attitude change
-        if (isPanelFound() && time > 20) {
+        if (isPanelFound()) {
             float velocity[3] = {0, 0.01, 0};
             ZRSetVelocityTarget(velocity);
         }
-        if (iHavePanel() && time > 70 && time < 75) {
+        if (iHavePanel())
+            state = 3;
+    }
+    if (state == 3) {
+        if (iHavePanel()) {
             float stop[3] = {0, 0, 0};
+            float velocity_difference;
             ZRSetVelocityTarget(stop);
+            velocity_difference = Vfunc(6, &myState[3], stop, NULL, 0);
+            if (velocity_difference < 0.02)
+                state = 4;
         }
-        if (iHavePanel() && time > 75 && time < 150) {
+    }
+    if (state == 4) {
+        if (iHavePanel()) {
             float station[3] = {-.7, 0, 0};
+            float station_attitude[3] = {0, 0, 1};
+            float station_distance;
             ZRSetPositionTarget(station);
+            ZRSetAttitudeTarget(station_attitude);
+            station_distance = Vfunc(6, myState, station, NULL, 0);
+            if (station_distance < 0.01)
+                state = 5;
         }
     }
-    if (iHavePanel() && time == 150) {
-        float searchStation[3] = {0, 0.1, 0};
-        ZRSetTorques(searchStation);
-    }
-    if (isStationInSync() && time > 150) {
-        float stop[3] = {0, 0, 0};
-        float velocity[3] = {0, 0, .01};
-        ZRSetTorques(stop);
-        ZRSetVelocityTarget(velocity);
+    if (state == 5) {
+        if (iHavePanel()) {
+            float station_attitude[3] = {0, 0, 1};
+            float station_position[3] = {-.7, 0, 0};
+            ZRSetPositionTarget(station_position);
+            ZRSetAttitudeTarget(station_attitude);
+        }
     }
 }
