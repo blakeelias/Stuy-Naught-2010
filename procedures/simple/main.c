ec41f5c16d03cd79a1eb4e6a50dfc853dcae1b38
abkjker <akbjker@gmail.com>
2010-10-11 13:15:02 -0400

    Added Vfunc defines... (You guys are going to kill me for this, aren't you?)

--- procedures/simple/main.c
+++ procedures/simple/main.c
@@ -2,6 +2,19 @@
 #define SET_POSITION_TARGET ZRSetPositionTarget
 #define SET_ATTITUDE_TARGET ZRSetAttitudeTarget
 
+//Vfunc defines...
+
+#define VLen(a)              Vfunc(0, (a), NULL, NULL, 0)
+#define VAdd(a, b, result)   Vfunc(1, (a), (b), (result), 0)
+#define VSub(a, b, result)   Vfunc(2, (a), (b), (result), 0)
+#define VUnit(a, result)     Vfunc(3, (a), NULL, (result), 0)
+#define VMult(a, b, result)  Vfunc(4, (a), NULL, (result), (b))
+#define VDot(a, b)           Vfunc(5, (a), (b), NULL, 0)
+#define VDist(a, b)          Vfunc(6, (a), (b), NULL, 0)
+#define VCopy(a, result)     Vfunc(7, (a), NULL, (result), 0)
+#define VAngle(a, b)         Vfunc(8, (a), (b), NULL, 0)
+#define VPoint(a, b, result) Vfunc(9, (a), (b), (result), 0)
+
 #define STATE_INIT        0
 #define STATE_PREP        1
 #define STATE_SEARCHING   2
