52563f401465ee9ffefc2864feab0ead85f85b8d
abkjker <akbjker@gmail.com>
2010-10-11 12:54:58 -0400

    Snail steps... (Snails have feet?)

--- procedures/simple/S_Panel.c
+++ procedures/simple/S_Panel.c
@@ -1,1 +1,11 @@
 //Code to move to panel...
+
+if (iHavePanel())
+{
+ state = STATE_TOSTATION;
+ break;
+}
+
+SET_POSITION_TARGET(panelState);
+
+//Insert math that points the satellite outwards...
--- procedures/simple/S_Search.c
+++ procedures/simple/S_Search.c
@@ -2,7 +2,11 @@
 
 #include "S_CheckPanel.c"
 
-SET_ATTITUDE_TARGET(&tangentPoints[3]);
+target[0] = tangentPoints[3] - myState[0];
+target[1] = tangentPoints[4] - myState[1];
+target[2] = tangentPoints[5] - myState[2];
+
+SET_ATTITUDE_TARGET(target);
 
 target[0] = getPanelSide() * 0.7;
 target[1] = myState[1];
