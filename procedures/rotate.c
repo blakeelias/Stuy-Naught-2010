aee465b4024cd4f417282d38d149350a97738ec8
Abhimanyu Ghosh <Aghosh993@gmail.com>
2010-10-12 01:52:17 -0400

    Added the alignToPlusY() function

--- procedures/rotate.c
+++ procedures/rotate.c
@@ -24,9 +24,24 @@ printf("%d, %f, %f, %f, %f \n", param_t, PI, set[0], set[1], set[2]);
 ZRSetAttitudeTarget(set);
 }
 
+void alignToPlusY() {
+	// Performs initial alignment to +Y axis prior to rotateTau()
+	// Requires a "stage" global variable initialized to 0;
+	// In my case, this came before stage 3, hence the assignment to the number 3;
+	
+	float attTarget[3] = {0.0, 1.0, 0.0};
+	ZRSetAttitudeTarget(attTarget);
+	if (Vfunc(8, att1Target, myState+6, NULL, 0) < 0.75) {
+		stage = 3; // Ties in to stage global variable effective / used in ZRUser().
+	}
+}
+
 void rotateTau() {
 	// Rotates about the x-axis by applying a torque of 0.025 N*m (?)
 	// Requires an int tauSet to be set to 1 and a float correctPos to be set to 1.0;
+	// Somewhat more unstable... depends on the previous performance of an attitude alignment being within a given bound
+	// Sometimes the above mentioned alignment exceeds given time limits..
+	// If alignment goes smoothly however, this function seems to work pretty well... but that's a big "if".
 	
 	float tau[3] = {0.0, 0.025, 0.0};
 	if (correctPos < 2) {
