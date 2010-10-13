24d3010a7dc29642f58cc8753f0db638828fb8a6
Blake Elias <blakeelias@gmail.com>
2010-10-12 22:45:33 -0400

    added simple-retrieve.csv, history data from simple-retrieve.c

--- strategies/simple-retrieve.c
+++ strategies/simple-retrieve.c
@@ -1,7 +1,7 @@
 /*
  * Simulation settings:
  *    Simulate as: SPH1
- *    Maximum Time: 210 seconds
+ *    Maximum Time: 20 seconds
  *    Game Variables:
  *        initangle: 0
  *        panelangle: 0
@@ -12,24 +12,28 @@
  *        SPH2: X=0,   Y=-0.3484, Z=0
  *
  * Result codes:
- *    SPH1 = 20 (I won)
- *    SPH2 = 40 (Other sat won)
+ *    SPH1 = 0
+ *    SPH2 = 0
  *
- * Total time: 94.0 seconds
- * Fuel used: 67% (33% remaining)
- *
- * Description of behavior:
- *    SPHERE 1 (blue) starts in the satellite initialization circle, moves
- *    to solar panel initialization circle, and immediately sees the panel
- *    (relies on initial panel position).  Moves forward, docks to panel,
- *    travels to station and docks panel to the station.
  * 
  */
 
 int state = 0;
+float history[7596];
+float procvar[1000];
+float G_time = 0;
 
 void ZRUser(float * myState, float * otherState, float time) {
-    printf("%d\n", state);
+    float history_portion[36];
+    int i;
+    HistFunc(0, myState, otherState, NULL, time);
+    HistFunc(1, history_portion, &history_portion[12], &history_portion[24], time);
+    
+    for(i = 0; i < 36; i++) {
+        printf("%f", history_portion[i]);
+        if (i % 36) printf(",");
+        else printf("\n");
+    }
     
     if (state == 0) {
         float attitude[3] = {0, 1, 0};
@@ -41,12 +45,35 @@ void ZRUser(float * myState, float * otherState, float time) {
         
         panel_center_distance = Vfunc(6, myState, panel_center, NULL, 0);
         if (panel_center_distance < 0.01)
+            state = 1;
+    }
+    if (state == 1) {
+        float panel_center[3] = {.7, 0, 0};
+        //float search[3] = {0, 0.01, 0};
+        float search_attitude[3] = {0, 0, -1};
+        ZRSetPositionTarget(panel_center);
+        ZRSetAttitudeTarget(search_attitude);
+        //ZRSetTorques(search);
+        if (isPanelFound())
             state = 2;
     }
-    //TODO: rotate around the circle to find the panel; in this case,
-    //panel angle is 0 so we automatically see it after the attitude change
-    if (state == 1);
     if (state == 2) {
+        float panel_center[3] = {.7, 0, 0};
+        ZRSetPositionTarget(panel_center);
+        if (isPanelFound()) {
+            HistRetrieve(&procvar[200], time, 6, 3);
+            ZRSetAttitudeTarget(&procvar[200]);
+            state = 3;
+        }
+    }
+    if (state == 3) {
+        float attitude_rate;
+        ZRSetAttitudeTarget(&procvar[200]);
+        attitude_rate = Vfunc(0, &procvar[200], NULL, NULL, 0);
+        if (isPanelFound() && attitude_rate < 0.01)
+            state = 4;
+    }
+    if (state == 4) {
         float attitude[3] = {0, 1, 0};
         ZRSetAttitudeTarget(attitude);
         if (isPanelFound()) {
@@ -54,9 +81,9 @@ void ZRUser(float * myState, float * otherState, float time) {
             ZRSetVelocityTarget(velocity);
         }
         if (iHavePanel())
-            state = 4;
+            state = 5;
     }
-    if (state == 4) {
+    if (state == 5) {
         if (iHavePanel()) {
             float station[3] = {-.7, 0, 0};
             float station_attitude[3] = {0, 0, 1};
@@ -65,10 +92,10 @@ void ZRUser(float * myState, float * otherState, float time) {
             ZRSetAttitudeTarget(station_attitude);
             station_distance = Vfunc(6, myState, station, NULL, 0);
             if (station_distance < 0.01)
-                state = 5;
+                state = 6;
         }
     }
-    if (state == 5) {
+    if (state == 6) {
         if (iHavePanel()) {
             float station_attitude[3] = {0, 0, 1};
             float station_position[3] = {-.7, 0, 0};
@@ -76,4 +103,6 @@ void ZRUser(float * myState, float * otherState, float time) {
             ZRSetAttitudeTarget(station_attitude);
         }
     }
+    
+    //G_time += 1;
 }
