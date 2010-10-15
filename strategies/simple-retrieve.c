8d3e4ca02be7d01601c3dfda5cd568ba83d72cf8
Blake Elias <blakeelias@gmail.com>
2010-10-15 01:49:26 -0400

    Working simple strategy, using center-rotation

--- strategies/simple-retrieve.c
+++ strategies/simple-retrieve.c
@@ -1,39 +1,72 @@
 /*
  * Simulation settings:
  *    Simulate as: SPH1
- *    Maximum Time: 20 seconds
- *    Game Variables:
- *        initangle: 0
- *        panelangle: 0
- *        panelradius: 0.3
- *        havepanel: 0
- *    Positioning:
- *        SPH1: X=0, Y=0.3484, Z=0
- *        SPH2: X=0,   Y=-0.3484, Z=0
+ *    Maximum Time: 210 seconds
+ *    Game Variables: generate from game
+ *    Positioning: set from game variables
  *
  * Result codes:
- *    SPH1 = 0
- *    SPH2 = 0
+ *    SPH1 = 20
+ *    SPH2 = 40
  *
- * 
+ * Other results:
+ *    Total time elapsed: 114 seconds
+ *    Fuel remaining: 18%
  */
 
 int state = 0;
-float history[7596];
 float procvar[1000];
-float G_time = 0;
+
+void RotateTarget(float * myState, float * pos) {
+    #define VLen(a)              Vfunc(0, (a), NULL, NULL, 0)
+    #define VAdd(a, b, result)   Vfunc(1, (a), (b), (result), 0)
+    #define VSub(a, b, result)   Vfunc(2, (a), (b), (result), 0)
+    #define VUnit(a, result)     Vfunc(3, (a), NULL, (result), 0)
+    #define VMult(a, b, result)  Vfunc(4, (a), NULL, (result), (b))
+    #define VDot(a, b)           Vfunc(5, (a), (b), NULL, 0)
+    #define VDist(a, b)          Vfunc(6, (a), (b), NULL, 0)
+    #define VCopy(a, result)     Vfunc(7, (a), NULL, (result), 0)
+    #define VAngle(a, b)         Vfunc(8, (a), (b), NULL, 0)
+    #define VPoint(a, b, result) Vfunc(9, (a), (b), (result), 0)
+
+    #define PI 3.14159265
+    #define Deg2Rad(Deg)	(Deg*PI/180.0)
+    #define Rad2Deg(Rad)	(Rad*180.0/PI)
+
+    // do ZRSetAttitudeTarget(current_att + AngleForward)
+    #define AngleForward	30
+
+    float temp[3], target_att[3], current_att[3];
+    float current_theta, target_theta;
+
+    ZRSetPositionTarget(pos);
+
+    VCopy(myState+6,current_att);
+    current_theta = atan2(current_att[2],current_att[1]);
+    if (current_theta < 0)
+        current_theta = current_theta + 2*PI;
+    target_theta = current_theta + Deg2Rad(AngleForward);
+
+    temp[0] = -5.0*current_att[0];
+    temp[1] = cos(target_theta);
+    temp[2] = sin(target_theta);
+    VUnit(temp,target_att);
+
+    //printf("time: %2.0f, current_att: (%5.2f, %5.2f, %5.2f), target: (%5.2f, %5.2f, %5.2f), to_target: %5.1f",
+    //procvar[0],current_att[0],current_att[1],current_att[2],target_att[0],target_att[1],target_att[2],
+    //VAngle(current_att,target_att));
+    //printf(" rates: (%5.2f, %5.2f, %5.2f)\n", myState[9],myState[10],myState[11]);
+    //printf( " out-of-plane: %5.2f\n",asin(current_att[0])*180/3.14159);
+
+    ZRSetAttitudeTarget(target_att);
+}
 
 void ZRUser(float * myState, float * otherState, float time) {
-    float history_portion[36];
-    int i;
-    HistFunc(0, myState, otherState, NULL, time);
-    HistFunc(1, history_portion, &history_portion[12], &history_portion[24], time);
-    
-    for(i = 0; i < 36; i++) {
-        printf("%f", history_portion[i]);
-        if (i-35) printf(",");
-        else printf("\n");
-    }
+    procvar[0] = time;
+    printf("time: %3.0f, state: %d, pos: (%5.2f, %5.2f, %5.2f), ",
+           time, state, myState[0], myState[1], myState[2]);
+    printf("att: (%5.2f, %5.2f, %5.2f), panel: %d, ",
+           myState[6], myState[7], myState[8], isPanelInSync());
     
     if (state == 0) {
         float attitude[3] = {0, 1, 0};
@@ -49,35 +82,40 @@ void ZRUser(float * myState, float * otherState, float time) {
     }
     if (state == 1) {
         float panel_center[3] = {.7, 0, 0};
-        //float search[3] = {0, 0.01, 0};
-        float search_attitude[3] = {0, 0, -1};
-        ZRSetPositionTarget(panel_center);
-        ZRSetAttitudeTarget(search_attitude);
-        //ZRSetTorques(search);
+        RotateTarget(myState, panel_center);
         if (isPanelFound())
             state = 2;
     }
     if (state == 2) {
         float panel_center[3] = {.7, 0, 0};
+        float panel_location[4];
         ZRSetPositionTarget(panel_center);
         if (isPanelFound()) {
-            HistRetrieve(&procvar[200], time, 6, 3);
-            ZRSetAttitudeTarget(&procvar[200]);
+            getPanelState(panel_location);
+            procvar[200] = panel_location[0];
+            procvar[201] = panel_location[1];
+            procvar[202] = panel_location[2];
+            Vfunc(9, myState, procvar+200, procvar+200, 0);
+            ZRSetAttitudeTarget(procvar+200);
             state = 3;
         }
     }
     if (state == 3) {
         float attitude_rate;
-        ZRSetAttitudeTarget(&procvar[200]);
-        attitude_rate = Vfunc(0, &procvar[200], NULL, NULL, 0);
-        if (isPanelFound() && attitude_rate < 0.01)
+        ZRSetAttitudeTarget(procvar+200);
+        attitude_rate = Vfunc(0, myState+9, NULL, NULL, 0);
+        printf("att_rate: %3.5f\n", attitude_rate);
+        if (isPanelFound())
+            procvar[300]++;
+        else procvar[300] = 0;
+        if (procvar[300] > 10)
             state = 4;
     }
     if (state == 4) {
-        float attitude[3] = {0, 1, 0};
-        ZRSetAttitudeTarget(attitude);
+        ZRSetAttitudeTarget(procvar+200);
         if (isPanelFound()) {
-            float velocity[3] = {0, 0.01, 0};
+            float velocity[3];
+            Vfunc(4, procvar+200, NULL, velocity, .01);
             ZRSetVelocityTarget(velocity);
         }
         if (iHavePanel())
@@ -103,6 +141,4 @@ void ZRUser(float * myState, float * otherState, float time) {
             ZRSetAttitudeTarget(station_attitude);
         }
     }
-    
-    //G_time += 1;
 }
