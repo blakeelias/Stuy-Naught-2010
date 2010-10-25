910ff3ad9272b1813f6b6ad54b29b3d1b9c64764
Blake Elias <blakeelias@gmail.com>
2010-10-24 21:28:14 -0400

    Failing to zap them

--- strategies/Tangent_Camp/S_Camp.c
+++ strategies/Tangent_Camp/S_Camp.c
@@ -5,7 +5,16 @@ target[1] = target[2] = 0;
 
 CoastToTarget(myState, target);
 
-target[0] = getPanelSide();
+if (VAngle(myState+6, otherState) < 40) {
+    VPoint(myState, otherState, target);
+    SET_ATTITUDE_TARGET(target);
+    ZRRepel();
+    DEBUG(("should be zapping\n"));
+}
+else {
+    target[0] = getPanelSide();
+    target[1] = target[2] = 0;
+    SET_ATTITUDE_TARGET(target);
+}
 
-SET_ATTITUDE_TARGET(target);
 
